e7386d94e960c6c5fb220361578c98209e9c08dc
anggunz <anggunzulaikha90@gmail.com>
2026-05-24 18:36:04 +0700

    Perubahan 5

--- src/bangundatar_202557201005/beranda.java
+++ src/bangundatar_202557201005/beranda.java
@@ -212,7 +212,8 @@ public class beranda extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main
+        (String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -231,7 +232,9 @@ public class beranda extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new beranda().setVisible(true));
+        java.awt.EventQueue.
+        invokeLater(()
+        -> new beranda().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
